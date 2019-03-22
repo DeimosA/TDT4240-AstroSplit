7beaf170a127eb2a38b6cd2f0f962d7d026c532e
lsnellingen <larssnellingen@hotmail.com>
2019-03-22 15:17:35 +0100

    Added a gameView with the engine and the worldEnitity

--- core/src/no/ntnu/tdt4240/astrosplit/World.java
+++ core/src/no/ntnu/tdt4240/astrosplit/World.java
@@ -1,4 +1,20 @@
 package no.ntnu.tdt4240.astrosplit;
 
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+
+import no.ntnu.tdt4240.astrosplit.views.GameView;
+
 public class World {
+
+	private World() {
+		Entity worldEntity = new Entity();
+		Engine engine = GameView.getGameEngine();
+		engine.addEntity(worldEntity);
+	}
+
+
+
+
 }
--- core/src/no/ntnu/tdt4240/astrosplit/views/GameView.java
+++ core/src/no/ntnu/tdt4240/astrosplit/views/GameView.java
@@ -1,4 +1,20 @@
 package no.ntnu.tdt4240.astrosplit.views;
 
+import com.badlogic.ashley.core.Engine;
+
 public class GameView {
+
+	private static Engine gameEngine = null;
+
+	private GameView() {
+
+	}
+	
+	public static Engine getGameEngine(){
+		if (gameEngine == null){
+			gameEngine = new Engine();
+		}
+		return gameEngine;
+	}
+
 }
