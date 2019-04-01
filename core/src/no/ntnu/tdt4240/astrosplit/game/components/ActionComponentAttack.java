4ade646f893261a03c61f109890159a80c4c03e5
Johannes Weiberg-Aurdal <johannes@dhcp-10-22-46-48.wlan.ntnu.no>
2019-04-01 15:34:34 +0200

    #5 Added attack and heal component classes

---
+++ core/src/no/ntnu/tdt4240/astrosplit/game/components/ActionComponentAttack.java
@@ -0,0 +1,8 @@
+package no.ntnu.tdt4240.astrosplit.game.components;
+
+import com.badlogic.ashley.core.Component;
+
+public class ActionComponentAttack implements Component {
+	public int damage = 25;
+	public int range = 10;
+}
