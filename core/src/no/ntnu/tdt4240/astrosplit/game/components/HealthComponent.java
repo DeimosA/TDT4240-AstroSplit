b9f3fc140c3aa2bbe07b44d3ce861cac3ad05015
lsnellingen <larssnellingen@hotmail.com>
2019-05-11 15:24:42 +0200

    Added a maxHealth in HelathComponent

--- core/src/no/ntnu/tdt4240/astrosplit/game/components/HealthComponent.java
+++ core/src/no/ntnu/tdt4240/astrosplit/game/components/HealthComponent.java
@@ -2,5 +2,6 @@ package no.ntnu.tdt4240.astrosplit.game.components;
 import com.badlogic.ashley.core.Component;
 
 public class HealthComponent implements Component{
+	public int maxHealth = 300;
 	public int health = 300;
 }
